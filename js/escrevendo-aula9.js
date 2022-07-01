c336431063a2aab5975177403b66136c1419dcd6
deyve853 <deyverson853@gmail.com>
2022-07-01 09:41:21 -0300

    entrevista

--- js/escrevendo-aula9.js
+++ js/escrevendo-aula9.js
@@ -3,7 +3,7 @@ var button = document.querySelector("button");
 button.onclick = function () {
     var nome = prompt("Qual é o comando para Listar os commits?");
     if ((nome == "git log")) {
-        alert(nome + ",Serve para listar os recursos da pasta.Parabéns você acertou!");
+        alert(nome + ",Serve para listar os commits.Parabéns você acertou!");
         window.location = "aula10.html";
     }
     else {
